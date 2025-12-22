cb1c83a12f7cc7babba395d02eaacbe0f1ff3087
Shivam24S <shivamshrivastav8055@gmail.com>
2025-12-22 17:33:58 +0530

    intro added

---
+++ 2.cinAndCout.cpp
@@ -0,0 +1,17 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "enter your age\n";
+
+    int age;
+
+    cin >> age;
+
+    cout << "your age is" << age;
+
+    return 0;
+}
